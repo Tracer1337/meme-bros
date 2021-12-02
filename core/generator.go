d5e1ac0f530e0212dc59375cc16c7a289528caa0
Tracer1337 <merlin.moelter@gmail.com>
2021-12-02 23:05:33 +0100

    fix(core): fix rect drawing

--- core/generator.go
+++ core/generator.go
@@ -25,23 +25,18 @@ type Element interface {
 
 func (c *Canvas) drawElements(dc *gg.Context) {
 	for _, e := range c.Elements.Images {
-		e.Draw(dc)
-		if c.Debug {
-			e.Rect.Draw(dc)
-		}
+		e.Draw(dc, c)
 	}
 
 	for _, e := range c.Elements.Textboxes {
-		e.Draw(dc)
-		if c.Debug {
-			e.Rect.Draw(dc)
-		}
+		e.Draw(dc, c)
 	}
 }
 
-func (e *ImageElement) Draw(dc *gg.Context) {
+func (e *ImageElement) Draw(dc *gg.Context, c *Canvas) {
 	defer dc.Identity()
 	defer dc.ResetClip()
+
 	img := utils.ParseBase64Image(e.Data.URI)
 	bounds := img.Bounds()
 	sx, sy := e.Rect.ApplyScaling(dc, float64(bounds.Max.X), float64(bounds.Max.Y))
@@ -49,10 +44,15 @@ func (e *ImageElement) Draw(dc *gg.Context) {
 	dc.DrawRoundedRectangle(e.Rect.X, e.Rect.Y, e.Rect.Width, e.Rect.Height, e.Data.BorderRadius)
 	dc.Clip()
 	dc.DrawImage(img, int(e.Rect.X*(1/sx)), int(e.Rect.Y*(1/sy)))
+
+	if c.Debug {
+		e.Rect.Draw(dc)
+	}
 }
 
-func (e *TextboxElement) Draw(dc *gg.Context) {
+func (e *TextboxElement) Draw(dc *gg.Context, c *Canvas) {
 	defer dc.Identity()
+
 	text := e.Data.Text
 	if e.Data.Caps {
 		text = strings.ToUpper(text)
@@ -62,6 +62,17 @@ func (e *TextboxElement) Draw(dc *gg.Context) {
 	dc.SetHexColor(e.Data.Color)
 	e.Rect.ApplyRotation(dc)
 	dc.DrawStringWrapped(text, e.Rect.X, e.Rect.Y, 0, 0, e.Rect.Width, LINE_SPACING, resolveTextAlign(e.Data.TextAlign))
+
+	if c.Debug {
+		e.Rect.Draw(dc)
+	}
+}
+
+func (rect *Rect) Draw(dc *gg.Context) {
+	dc.SetColor(color.Black)
+	dc.SetLineWidth(3)
+	dc.DrawRectangle(rect.X, rect.Y, rect.Width, rect.Height)
+	dc.Stroke()
 }
 
 func (rect *Rect) ApplyRotation(dc *gg.Context) {
@@ -74,13 +85,6 @@ func (rect *Rect) ApplyScaling(dc *gg.Context, pWidth, pHeight float64) (sx, sy 
 	sy = rect.Height / float64(pHeight)
 	dc.Scale(sx, sy)
 	return sx, sy
-}
-
-func (rect *Rect) Draw(dc *gg.Context) {
-	dc.SetColor(color.Black)
-	dc.SetLineWidth(3)
-	dc.DrawRectangle(rect.X, rect.Y, rect.Width, rect.Height)
-	dc.Stroke()
 }
 
 func (rect *Rect) Center() (x float64, y float64) {
