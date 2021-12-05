33db91b1a0ad72780e5911cd97c6eac6e4a94128
Tracer1337 <merlin.moelter@gmail.com>
2021-12-05 17:07:33 +0100

    fix(core): fix scaling when drawing image

--- core/core/image.go
+++ core/core/image.go
@@ -8,10 +8,9 @@ import (
 )
 
 type ImageDrawingContext struct {
-	dc     *gg.Context
-	c      *Canvas
-	Img    image.Image
-	SX, SY float64
+	dc  *gg.Context
+	c   *Canvas
+	Img image.Image
 }
 
 func (e *ImageElement) Draw(dc *gg.Context, c *Canvas) {
@@ -24,7 +23,7 @@ func (e *ImageElement) Draw(dc *gg.Context, c *Canvas) {
 		Img: utils.ParseBase64Image(e.Data.URI),
 	}
 
-	e.applyTransform(context)
+	e.Rect.ApplyRotation(dc)
 	e.drawBorderRadius(context)
 	e.drawImage(context)
 
@@ -33,19 +32,14 @@ func (e *ImageElement) Draw(dc *gg.Context, c *Canvas) {
 	}
 }
 
-func (e *ImageElement) applyTransform(c *ImageDrawingContext) {
-	e.Rect.ApplyRotation(c.dc)
-	bounds := c.Img.Bounds()
-	sx, sy := e.Rect.ApplyScaling(c.dc, float64(bounds.Max.X), float64(bounds.Max.Y))
-	c.SX = sx
-	c.SY = sy
-}
-
 func (e *ImageElement) drawBorderRadius(c *ImageDrawingContext) {
 	c.dc.DrawRoundedRectangle(e.Rect.X, e.Rect.Y, e.Rect.Width, e.Rect.Height, e.Data.BorderRadius)
 	c.dc.Clip()
 }
 
 func (e *ImageElement) drawImage(c *ImageDrawingContext) {
-	c.dc.DrawImage(c.Img, int(e.Rect.X*(1/c.SX)), int(e.Rect.Y*(1/c.SY)))
+	bounds := c.Img.Bounds()
+	sx, sy := e.Rect.ApplyScaling(c.dc, float64(bounds.Max.X), float64(bounds.Max.Y))
+	c.dc.DrawImage(c.Img, int(e.Rect.X*(1/sx)), int(e.Rect.Y*(1/sy)))
+	c.dc.Scale(1/sx, 1/sy)
 }
