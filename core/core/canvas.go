9ebc8e4e46fc99aeb58324fe82fc20b37f808c66
Tracer1337 <merlin.moelter@gmail.com>
2021-12-06 23:10:35 +0100

    refactor(core): add elements collection

--- core/core/canvas.go
+++ core/core/canvas.go
@@ -6,6 +6,10 @@ import (
 	"github.com/fogleman/gg"
 )
 
+type Drawable interface {
+	Draw(dc *gg.Context, c *Canvas)
+}
+
 func (c *Canvas) Generate() *bytes.Buffer {
 	dc := gg.NewContext(int(c.Width), int(c.Height))
 	dc.SetColor(c.BackgroundColor)
@@ -17,15 +21,25 @@ func (c *Canvas) Generate() *bytes.Buffer {
 }
 
 func (c *Canvas) drawElements(dc *gg.Context) {
+	elements := c.collectElements()
+
+	for _, e := range elements {
+		e.Draw(dc, c)
+	}
+}
+
+func (c *Canvas) collectElements() []Drawable {
+	elements := make([]Drawable, 0)
+
 	for _, e := range c.Elements.Images {
-		e.Draw(dc, c)
+		elements = append(elements, e)
 	}
-
 	for _, e := range c.Elements.Textboxes {
-		e.Draw(dc, c)
+		elements = append(elements, e)
+	}
+	for _, e := range c.Elements.Shapes {
+		elements = append(elements, e)
 	}
 
-	for _, e := range c.Elements.Shapes {
-		e.Draw(dc, c)
-	}
+	return elements
 }
