2c646e4aaecbdea5f0f855c01ea9c108ba320ca8
Tracer1337 <merlin.moelter@gmail.com>
2021-12-17 21:08:29 +0100

    feat(mobile): fix image import

--- mobile/src/components/editor/utils/useBridge.ts
+++ mobile/src/components/editor/utils/useBridge.ts
@@ -1,12 +1,16 @@
 import { RefObject, useContext, useEffect, useRef } from "react"
 import WebView, { WebViewMessageEvent } from "react-native-webview"
+import { DeepPartial } from "tsdef"
 import EventEmitter from "../../../lib/EventEmitter"
 import { setListeners } from "../../../lib/events"
+import { importImage } from "../../../lib/media"
 import { Canvas, CanvasElement } from "../../../types"
 import { EditorContext } from "../Context"
 
 type Events = {
-    "element.create": CanvasElement["type"],
+    "element.create": DeepPartial<CanvasElement> & {
+        type: CanvasElement["type"]
+    },
     "canvas.render": null
 }
 
@@ -19,6 +23,26 @@ type Event<T extends keyof Events> = {
     data: Events[T]
 }
 
+async function createPartialElement(type: CanvasElement["type"]) {
+    const newElement: Events["element.create"] = { type }
+    if (type === "image") {
+        const image = await importImage()
+        if (!image || !image.base64) {
+            return
+        }
+        newElement.data = {
+            uri: image.base64,
+            animated: image.base64.startsWith("data:image/gif")
+        }
+        if (image.width && image.height) {
+            newElement.rect = {}
+            newElement.rect.width = newElement.data.naturalWidth = image.width
+            newElement.rect.height = newElement.data.naturalHeight = image.height
+        }
+    }
+    return newElement
+}
+
 export function useBridge(webview: RefObject<WebView>) {
     const context = useContext(EditorContext)
 
@@ -29,13 +53,17 @@ export function useBridge(webview: RefObject<WebView>) {
         events.emit(data.type, data.data)
     }
 
-    const handleElementCreate = (type: CanvasElement["type"]) => {
+    const handleElementCreate = async (type: CanvasElement["type"]) => {
         if (!webview.current) {
+            return
+        }
+        const data = await createPartialElement(type)
+        if (!data) {
             return
         }
         const message: Event<"element.create"> = {
             type: "element.create",
-            data: type
+            data
         }
         webview.current.postMessage(JSON.stringify(message))
     }
