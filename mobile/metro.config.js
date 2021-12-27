553fd3526994d179cd374b96559ea208c24ac76b
Tracer1337 <merlin.moelter@gmail.com>
2021-12-27 17:23:32 +0100

    fix(mobile): fix monorepo arch in react-native

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,9 +1,7 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const exclusionList = require("metro-config/src/defaults/exclusionList");
+const { getMetroTools } = require("react-native-monorepo-tools");
+
+const monorepoMetroTools = getMetroTools();
 
 module.exports = {
   transformer: {
@@ -13,5 +11,10 @@ module.exports = {
         inlineRequires: true,
       },
     }),
+  },
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    blockList: exclusionList(monorepoMetroTools.blockList),
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
   },
 };
