8aee7594a0afc9ddc6563ee4542b3c4aa8f73184
Tracer1337 <merlin.moelter@gmail.com>
2021-12-30 12:59:33 +0100

    feat(core): remove multi-threading from element rendering

--- core/core/render.go
+++ core/core/render.go
@@ -5,94 +5,23 @@ import (
 )
 
 type RenderingContext struct {
-	Canvas   *Canvas
-	NLayers  int
-	Layers   []Drawable
-	Rendered []*gg.Context
-	Merged   []bool
+	Canvas *Canvas
 }
 
 func NewRenderingContext(c *Canvas) *RenderingContext {
-	n := len(c.Drawables)
 	return &RenderingContext{
-		Canvas:   c,
-		NLayers:  n,
-		Layers:   c.Drawables,
-		Rendered: make([]*gg.Context, n),
-		Merged:   make([]bool, n),
+		Canvas: c,
 	}
 }
 
 func (rc *RenderingContext) Render(index int) *gg.Context {
-	done := make(chan int)
+	l := rc.newLayer()
 
-	rc.drawLayers(done, index)
-	rc.mergeLoop(done)
-
-	return rc.Rendered[0]
-}
-
-func (rc *RenderingContext) drawLayers(done chan int, index int) {
-	for i, e := range rc.Layers {
-		go func(i int, e Drawable) {
-			l := rc.newLayer()
-			e.Draw(l, rc.Canvas, index)
-			rc.Rendered[i] = l
-			done <- 0
-		}(i, e)
-	}
-}
-
-func (rc *RenderingContext) mergeLoop(done chan int) {
-	for range rc.Layers {
-		<-done
-		rc.mergeLayers()
+	for _, e := range rc.Canvas.Drawables {
+		e.Draw(l, rc.Canvas, index)
 	}
 
-	for rc.hasUnmergedLayers() {
-		rc.mergeLayers()
-	}
-}
-
-func (rc *RenderingContext) mergeLayers() {
-	for j := 0; j < rc.NLayers; j++ {
-		if rc.Rendered[j] == nil {
-			continue
-		}
-
-		k := rc.findNextLayer(j)
-		if k == -1 {
-			continue
-		}
-
-		rc.Rendered[j].DrawImage(rc.Rendered[k].Image(), 0, 0)
-		rc.Merged[k] = true
-		rc.Rendered[k] = nil
-	}
-}
-
-func (rc *RenderingContext) findNextLayer(i int) int {
-	for j := i + 1; j < rc.NLayers; j++ {
-		if rc.Rendered[j] == nil && !rc.Merged[j] {
-			return -1
-		}
-		if rc.Rendered[j] != nil {
-			return j
-		}
-	}
-	return -1
-}
-
-func (rc *RenderingContext) hasUnmergedLayers() bool {
-	if len(rc.Merged) <= 1 {
-		return false
-	}
-	for i := 1; i < len(rc.Merged); i++ {
-		if !rc.Merged[i] {
-			return true
-		}
-	}
-	return false
+	return l
 }
 
 func (rc *RenderingContext) newLayer() *gg.Context {
