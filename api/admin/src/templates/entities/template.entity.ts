9b6b7f857d32c74f873984d3b925d9a8b73f7dce
Tracer1337 <merlin.moelter@gmail.com>
2022-01-08 13:18:57 +0100

    feat(admin-api): add canvas to template schema

--- api/admin/src/templates/entities/template.entity.ts
+++ api/admin/src/templates/entities/template.entity.ts
@@ -1,10 +1,15 @@
 import { TemplateDocument } from "../schemas/template.schema"
 
 export class TemplateEntity {
+    id: string
+
     name: string
+
+    canvas: object
 
     constructor(document: TemplateDocument) {
         const object = document.toObject()
+        this.id = object._id.toString()
         delete object._id
         delete object.__v
         Object.assign(this, object)
--- api/admin/src/templates/schemas/template.schema.ts
+++ api/admin/src/templates/schemas/template.schema.ts
@@ -1,12 +1,18 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
-import { Document } from "mongoose"
+import { Document, SchemaTypes } from "mongoose"
 
 export type TemplateDocument = Template & Document
 
 @Schema()
 export class Template {
-    @Prop()
+    @Prop({ required: true })
     name: string
+
+    @Prop({
+        required: true,
+        type: SchemaTypes.Mixed
+    })
+    canvas: object
 }
 
 export const TemplateSchema = SchemaFactory.createForClass(Template)
