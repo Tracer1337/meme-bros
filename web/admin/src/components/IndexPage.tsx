513ec572233e57a65a208834ca5f7e83d8d6ce71
Tracer1337 <merlin.moelter@gmail.com>
2022-01-13 19:24:33 +0100

    feat(web/admin): add index page

---
+++ web/admin/src/components/IndexPage.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+import React from "react"
+
+function IndexPage() {
+    return (
+        <Typography variant="h3" mx={2} mt={4}>
+            Admin
+        </Typography>
+    )
+}
+
+export default IndexPage
--- web/admin/src/components/router/Router.tsx
+++ web/admin/src/components/router/Router.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { Typography } from "@mui/material"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "../layout/Layout"
 import TemplatesPage from "../templates/TemplatesPage"
 import RequireAuth from "../auth/RequireAuth"
 import LoginPage from "../auth/LoginPage"
 import ChangePasswordPage from "../auth/ChangePasswordPage"
+import IndexPage from "../IndexPage"
 
 function auth(child: JSX.Element) {
     return (
@@ -20,9 +20,7 @@ function Router() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Layout/>}>
-                    <Route index element={
-                        auth(<Typography variant="h3">Admin</Typography>)
-                    }/>
+                    <Route index element={auth(<IndexPage/>)}/>
                     <Route path="templates" element={auth(<TemplatesPage/>)}/>
                     <Route path="login" element={<LoginPage/>}/>
                     <Route path="change-password" element={auth(<ChangePasswordPage/>)}/>
