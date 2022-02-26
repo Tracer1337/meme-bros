2c8f2c76c28af01a113bf5d593f8cbcb0ce790ef
Tracer1337 <merlin.moelter@gmail.com>
2022-02-26 21:42:19 +0100

    feat(app): add remove action to common element actions

--- app/src/components/editor/CommonElementActions.tsx
+++ app/src/components/editor/CommonElementActions.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import {
     copyElement,
     layerElement,
+    removeElement,
     useSharedContext
 } from "@meme-bros/client-lib"
 import { useActions } from "./utils/actions"
@@ -15,9 +16,10 @@ function CommonElementActions({ action }: {
 
     return (
         <>
-            {action("content-copy", () => context.set(copyElement(context, id)))}
+            {action("delete-outline", () => context.set(removeElement(context, id)))}
             {action("flip-to-back", () => context.set(layerElement(context, id, -1)))}
             {action("flip-to-front", () => context.set(layerElement(context, id, 1)))}
+            {action("content-copy", () => context.set(copyElement(context, id)))}
         </>
     )
 }
