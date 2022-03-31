8205596f0db60318eca3acffd15833e76f26e02b
Tracer1337 <merlin.moelter@gmail.com>
2022-03-31 19:10:54 +0200

    feat(mobile): update metro config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,24 +1,16 @@
 const exclusionList = require("metro-config/src/defaults/exclusionList");
-const { getMetroTools, getMetroAndroidAssetsResolutionFix } = require("react-native-monorepo-tools");
+const { getMetroTools } = require("react-native-monorepo-tools");
 
 const monorepoMetroTools = getMetroTools();
 
-const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
-
 module.exports = {
   transformer: {
-    publicPath: androidAssetsResolutionFix.publicPath,
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: false,
       },
     }),
-  },
-  server: {
-    enhanceMiddleware: (middleware) => {
-      return androidAssetsResolutionFix.applyMiddleware(middleware);
-    },
   },
   watchFolders: monorepoMetroTools.watchFolders,
   resolver: {
