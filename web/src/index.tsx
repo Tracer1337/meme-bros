be50d5568559c899bc2f656ab3e98489d8bfcf8d
Tracer1337 <merlin.moelter@gmail.com>
2022-04-28 20:15:27 +0200

    chore(web): remove api authentication

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,14 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { APIProvider } from "@meme-bros/api-sdk"
 import { BridgeProvider, SharedContextProvider } from "@meme-bros/client-lib"
-import { Storage } from "./lib/storage"
 import App from "./App"
 
 ReactDOM.render(
     <React.StrictMode>
         <APIProvider config={{
-            host: process.env.ADMIN_API_HOST || "http://localhost:5000",
-            token: Storage.get(Storage.Keys.TOKEN) || ""
+            host: process.env.ADMIN_API_HOST || "http://localhost:5000"
         }}>
             <BridgeProvider>
                 <SharedContextProvider>
